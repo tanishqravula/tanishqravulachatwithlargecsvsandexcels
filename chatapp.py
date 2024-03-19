e4ceee9caffec481294dd6b78cf94ba077d6757c
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2024-03-19 21:23:01 +0530

    Update chatapp.py

--- chatapp.py
+++ chatapp.py
@@ -83,7 +83,7 @@ def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 
     try:
-        new_db = FAISS.load_local("faiss_index", embeddings)  # Set allow_dangerous_deserialization=True
+        new_db = FAISS.load_local("faiss_index", embeddings,allow_dangerous_deserialization=True)  # Set allow_dangerous_deserialization=True
         docs = new_db.similarity_search(user_question)
 
         chain = get_conversational_chain()
